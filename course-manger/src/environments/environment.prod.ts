0dd209a41271b35ae2119faba28f3b13f3846eaf
Natalia Cristine <naticristinenunes@gmail.com>
2021-01-23 22:04:15 -0300

    removendo chaves

--- course-manger/src/environments/environment.prod.ts
+++ course-manger/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey:'06664ebdca42539f05d8e9dba81780a7',
+  apiKey:'chave key',
 };
--- course-manger/src/environments/environment.ts
+++ course-manger/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: '06664ebdca42539f05d8e9dba81780a7',
+  apiKey: 'chave key',
 };
 
 /*
